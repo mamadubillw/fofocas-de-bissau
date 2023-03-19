9886ad727cc1fa2e38b5bb7556bd97d0b3272f90
Mamadu Billo Djalo <mamadubdj@gmail.com>
2023-03-19 13:35:53 +0100

    Graphql d fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,38 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
-const Home: NextPage = () => {
+import { PostCard, Categories, PostWidget } from '../components';
+import {getPosts} from '../services';
+
+const posts = [
+  {title: 'React Testing', excerpt: 'React React Testing'},
+  {title: 'React whit tailwind', excerpt: 'Lear React whit tailwind'}
+];
+
+export default function Home({posts}) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <div className="container mx-auto px-10 mb-8">
+  
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title}/>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-      )
+    </div>
+  );
 }
 
-export default Home
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {posts}
+  }
+}
